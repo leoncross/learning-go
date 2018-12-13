35b37ccca49483ef062f2da179f4937513b40595
leoncross <leon@lscross.co.uk>
2018-12-13 10:35:32 +0000

    arrays and slices; passing tests

--- ArraysAndSlices/sum.go
+++ ArraysAndSlices/sum.go
@@ -1,6 +1,7 @@
 package main
 
 // Sum combines numbers of an array
+// and fixes size of array (5)
 func Sum(numbers [5]int) int {
 	sum := 0
 	for i := 0; i < 5; i++ {
@@ -8,6 +9,16 @@ func Sum(numbers [5]int) int {
 	}
 	return sum
 
+}
+
+// Sum2 is same as Sum, but different layout
+// and is a slice (allows any size array)
+func Sum2(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
 
 func main() {}
--- ArraysAndSlices/sum_test.go
+++ ArraysAndSlices/sum_test.go
@@ -3,12 +3,38 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("basic way of adding in an array", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+func TestSum2(t *testing.T) {
+	t.Run("refactored way of summing an array", func(t *testing.T) {
+		numbers := []int{1, 1, 1, 2, 2}
+
+		got := Sum2(numbers)
+		want := 7
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("Can sum any size of array", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum2(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
+
 }
